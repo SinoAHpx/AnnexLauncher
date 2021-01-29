b94ff6f80e8c17db4999958ae419f3a090e4ae64
AHpx <ahpx@yandex.com>
2021-01-30 02:02:07 +0800

    添加了HomeView

---
+++ View/HomeView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignColors;
+using MaterialDesignColors.ColorManipulation;
+using MaterialDesignThemes.Wpf;
+
+namespace AnnexLauncher.View
+{
+    /// <summary>
+    /// HomeView.xaml 的交互逻辑
+    /// </summary>
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var helper = new PaletteHelper();
+            var theme = helper.GetTheme();
+
+            theme.PrimaryLight = new ColorPair(Colors.White.Lighten());
+            theme.PrimaryMid = new ColorPair(Colors.White);
+            theme.PrimaryDark = new ColorPair(Colors.White.Darken());
+
+            HomeTheme.SetTheme(theme);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            PopupBoxx.IsPopupOpen = true;
+        }
+    }
+}
--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -27,6 +29,7 @@ namespace AnnexLauncher
 
             DataContext = new MainWindowVM();
         }
+
 
         private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
